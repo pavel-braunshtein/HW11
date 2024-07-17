ec994e68cde14b32aae8ca5d64a94e0992627480
pavelbraun333 <pavelbraun333@gmail.com>
2024-07-17 12:08:25 +0300

    project has been completed

--- Lesson11HW/Lesson11HW/Source/Screen/ContactUsViewController.swift
+++ Lesson11HW/Lesson11HW/Source/Screen/ContactUsViewController.swift
@@ -9,8 +9,62 @@ import UIKit
 
 class ContactUsViewController: BaseViewController {
     
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textView.delegate = self
     }
+    
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+        print("submitButtonPressed is pressed")
+        textView.resignFirstResponder()
+    }
+    
+    func updateLabel (amount: Int) {
+        
+        let remainingSymbols = max(0, 320 - amount)
+            countLabel.text = "\(remainingSymbols) symbols left"
+    }
+    
 }
 
+extension ContactUsViewController: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+//        let newText = (textView.text as NSString).replacingCharacters(in: range, with: text)
+        let newText = (textView.text ?? "") + text
+        
+        if newText.isEmpty {
+            
+            textLabel.isHidden = false
+        } else {
+            textLabel.isHidden = true
+        }
+        
+        if newText.count >= 15 && newText.count <= 320 {
+    
+            submitButton.isEnabled = true
+            updateLabel(amount: newText.count)
+            return true
+        } else {
+            
+            if newText.count >= 320 {
+                let trimmedText = String(newText.prefix(320))
+                textView.text = trimmedText
+                submitButton.isEnabled = true
+            } else {
+                submitButton.isEnabled = false
+            }
+            
+            updateLabel(amount: newText.count)
+            return newText.count <= 320
+        }
+    }
+    
+}
--- Lesson11HW/Lesson11HW/Source/Screen/ProfileViewController.swift
+++ Lesson11HW/Lesson11HW/Source/Screen/ProfileViewController.swift
@@ -11,9 +11,18 @@ class ProfileViewController: BaseViewController {
     
     private var editMode = false
     
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var firstNameSymbols: UILabel!
+    @IBOutlet weak var lastNameSymbols: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupActions()
+        
+        firstName.delegate = self
+        lastName.delegate = self
     }
 }
 
@@ -27,7 +36,7 @@ private extension ProfileViewController {
     func updateRigthtBarButton() {
         
         let title = editMode ? "Done" : "Edit"
-
+        
         let barButton = UIBarButtonItem(
             title: title,
             style: .plain,
@@ -42,6 +51,48 @@ private extension ProfileViewController {
         debugPrint("ProfileViewController -> changeModeButtonDidTap")
         
         editMode = !editMode
+        firstName.isEnabled = editMode
+        lastName.isEnabled = editMode
+        firstNameSymbols.isHidden = !firstNameSymbols.isHidden
+        lastNameSymbols.isHidden = !lastNameSymbols.isHidden
+        
         updateRigthtBarButton()
     }
+    
+    func updateLabel (symbolsNum: Int, label: UILabel) {
+        
+        if symbolsNum >= 30 {
+            
+            label.text = "\(30 - symbolsNum) symbols left"
+            label.textColor = .red
+        } else {
+            label.text = "\(30 - symbolsNum) symbols left"
+            label.textColor = .green
+        }
+    }
 }
+
+    extension ProfileViewController: UITextFieldDelegate {
+        
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            
+            guard let currentText = textField.text else {
+                return true
+            }
+//            let newText = (currentText as NSString).replacingCharacters(in: range, with: string)
+            let newText = currentText + string
+
+            
+            if newText.count <= 30 {
+                
+                if textField == firstName {
+                    updateLabel(symbolsNum: newText.count, label: firstNameSymbols)
+                } else {
+                    updateLabel(symbolsNum: newText.count, label: lastNameSymbols)
+                }
+                return true
+            } else {
+                return false
+            }
+        }
+    }
